6aadd13d18afc49593b022d966f36ae4e03e4b61
varalakshmi <varalakshmi@77.com>
2022-07-01 06:16:07 +0000

    create anther file

---
+++ index.js
@@ -0,0 +1,8 @@
+const className = "greeting";
+const reactElement = (
+  <div>
+    <h1 className={className}>Hello ,Rahul</h1>
+    <p>Good to see you </p>
+  </div>
+);
+ReactDOM.render(reactElement, document.getElementById("root"));
